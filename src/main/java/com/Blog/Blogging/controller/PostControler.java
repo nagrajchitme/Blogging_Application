89c9f77bfb2b0b6f3441cc8f4f894b149df4d01c
Nagraj Chitme <161920374+nagrajchitme@users.noreply.github.com>
2025-04-27 11:58:51 +0530

    Update PostControler.java
    some little changes

--- src/main/java/com/Blog/Blogging/controller/PostControler.java
+++ src/main/java/com/Blog/Blogging/controller/PostControler.java
@@ -1,53 +1,65 @@
 package com.Blog.Blogging.controller;
 
 import com.Blog.Blogging.model.post;
+import com.Blog.Blogging.model.post;
 import com.Blog.Blogging.repository.PostRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
-public class PostControler {
+public class PostController {
 
     @Autowired
-    PostRepo repo;
+    private PostRepo postRepo;
 
+    // Home page showing all posts
     @GetMapping("/")
-   public String viewHomePage(Model model){
-       model.addAttribute("listPost",repo.findAll());
-       return "index";
-   }
+    public String viewHomePage(Model model) {
+        model.addAttribute("listPost", postRepo.findAll());
+        return "index";
+    }
 
-   @GetMapping("/new")
-   public String newPost(Model model){
-        model.addAttribute("post",new post());
+    // Show form to create a new post
+    @GetMapping("/new")
+    public String showNewPostForm(Model model) {
+        model.addAttribute("post", new post());
         return "new_post";
-   }
+    }
 
-    @GetMapping("/save")
-    public String savePost(@ModelAttribute("post") post post){
-        repo.save(post);
+    // Save a new post (use POST method)
+    @PostMapping("/save")
+    public String savePost(@ModelAttribute("post") post post) {
+        postRepo.save(post);
         return "redirect:/";
     }
 
+    // Show form to edit an existing post
     @GetMapping("/edit/{id}")
-    public String editPost(@PathVariable int id,Model model){
-        model.addAttribute("post", repo.findById(id));
-        return "edit_post";
+    public String showEditPostForm(@PathVariable("id") int id, Model model) {
+        Optional<post> post = postRepo.findById(id);
+        if (post.isPresent()) {
+            model.addAttribute("post", post.get());
+            return "edit_post";
+        } else {
+            return "redirect:/"; // You can redirect to an error page if needed
+        }
     }
 
-    @GetMapping("/update")
-    public String updatePost(@ModelAttribute("post") post post){
-        repo.save(post);
+    // Update a post (use POST method)
+    @PostMapping("/update")
+    public String updatePost(@ModelAttribute("post") post post) {
+        postRepo.save(post);
         return "redirect:/";
     }
 
+    // Delete a post by ID
     @GetMapping("/delete/{id}")
-    public String deletePost(@PathVariable int id){
-        repo.deleteById(id);
+    public String deletePost(@PathVariable("id") int id) {
+        postRepo.deleteById(id);
         return "redirect:/";
     }
 }
